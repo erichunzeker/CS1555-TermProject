944c74e0293265f714df75b56672e35590a4574c
Eric Hunzeker <emh128@pitt.edu>
2019-02-25 12:25:53 -0500

    start ddl

--- sql/phase1.sql
+++ sql/phase1.sql
@@ -1,1 +1,38 @@
 -- insert ddl commands here
+
+CREATE TABLE station (
+  station_id      INTEGER,
+  address         VARCHAR,
+
+);
+
+CREATE TABLE railline (
+  line_id         INTEGER,
+
+);
+
+CREATE TABLE route ();
+
+CREATE TABLE schedule ();
+
+CREATE TABLE train (
+
+);
+
+CREATE TABLE passenger (
+  passenger_id    INTEGER,
+  firstname       VARCHAR(30),
+  lastname        VARCHAR(50),
+  phone           VARCHAR(15),
+  street          VARCHAR(50),
+  city            VARCHAR(50),
+  state           VARCHAR(50),
+  zip             VARCHAR(50),
+  country         VARCHAR(50),
+  email           VARCHAR(50),
+
+  CONSTRAINT Passengers_PK
+    PRIMARY KEY (passenger_id)
+);
+
+----------------------------
