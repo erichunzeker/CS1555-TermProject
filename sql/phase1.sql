6dc6bf0d4f9a86a0cac3e20eb99b114269fcff06
Eric Hunzeker <emh128@pitt.edu>
2019-02-26 11:15:19 -0500

    finished phase1sql possbly

--- sql/phase1.sql
+++ sql/phase1.sql
@@ -1,39 +1,44 @@
--- insert ddl commands here
+DROP TABLE IF EXISTS station CASCADE;
+DROP TABLE IF EXISTS railline CASCADE;
+DROP TABLE IF EXISTS route CASCADE;
+DROP TABLE IF EXISTS schedule CASCADE;
+DROP TABLE IF EXISTS train CASCADE;
+DROP TABLE IF EXISTS passenger CASCADE;
 
 CREATE TABLE station (
-  station_id      INTEGER PRIMARY KEY,
+  station_id      INT,
   address         VARCHAR(100),
   opentime        TIME NOT NULL,
-  closetime       TIME NOT NULL
+  closetime       TIME NOT NULL,
+  PRIMARY KEY(station_id)
 );
 
 CREATE TABLE railline (
-  railline_id     INTEGER PRIMARY KEY,
-  speedlimit      INTEGER
+  railline_id     INT,
+  speedlimit      INT,
+  PRIMARY KEY(railline_id)
 );
 
 CREATE TABLE station_railline (
-  CONSTRAINT Station_FK
-    FOREIGN KEY (Station_ID) REFERENCES station(station_id),
-
-  CONSTRAINT Railline_FK
-    FOREIGN KEY (Railline_ID) REFERENCES railline(railline_id)
+  PRIMARY KEY(Station_ID, Railline_ID),
+  FOREIGN KEY (Station_ID) REFERENCES station(station_id),
+  FOREIGN KEY (Railline_ID) REFERENCES railline(railline_id)
 );
 
 CREATE TABLE route (
-  route_id        INTEGER PRIMARY KEY
+  route_id        INT,
+  PRIMARY KEY(route_id)
 );
 
 CREATE TABLE railline_route (
-  CONSTRAINT Railline_FK
-    FOREIGN KEY (Railline_ID) REFERENCES railline(railline_id),
-
-  CONSTRAINT Route_FK
-    FOREIGN KEY (Route_ID) REFERENCES route(route_id)
+  PRIMARY KEY(Railline_ID, Route_ID),
+  FOREIGN KEY (Railline_ID) REFERENCES railline(railline_id),
+  FOREIGN KEY (Route_ID) REFERENCES route(route_id)
 );
 
 CREATE TABLE schedule (
- schedule_id      INTEGER PRIMARY KEY,
+ schedule_id      INT,
+ PRIMARY KEY (schedule_id),
 
  CONSTRAINT Schedule_Route_FK
   FOREIGN KEY (Route_ID) REFERENCES route(route_id),
@@ -43,16 +48,17 @@ CREATE TABLE schedule (
 );
 
 CREATE TABLE train (
-  train_id        INTEGER PRIMARY KEY,
-  topspeed        INTEGER,
-  seats           INTEGER,
-  pricepermile    DECIMAL(4,2)
+  train_id        INT,
+  topspeed        INT,
+  seats           INT,
+  pricepermile    DECIMAL(4,2),
+  PRIMARY KEY(train_id)
 );
 
 CREATE TABLE passenger (
-  passenger_id    INTEGER PRIMARY KEY,
-  firstname       VARCHAR(30),
-  lastname        VARCHAR(30),
+  passenger_id    SERIAL,
+  firstname       VARCHAR(20),
+  lastname        VARCHAR(20),
   phone           VARCHAR(15),
   street          VARCHAR(50),
   city            VARCHAR(50),
@@ -60,21 +66,25 @@ CREATE TABLE passenger (
   zip             VARCHAR(15),
   country         VARCHAR(20),
   email           VARCHAR(35),
-
-  CONSTRAINT Passengers_PK
-    PRIMARY KEY (passenger_id)
+  PRIMARY KEY(passenger_id)
 );
 
 CREATE TABLE train_passenger (
-  CONSTRAINT Train_FK
-    FOREIGN KEY (Train_ID) REFERENCES train(train_id),
-
-  CONSTRAINT Passenger_FK
-    FOREIGN KEY (Passenger_ID) REFERENCES passenger(passenger_id)
+  PRIMARY KEY(Train_ID, Passenger_ID)
+  FOREIGN KEY (Train_ID) REFERENCES train(train_id),
+  FOREIGN KEY (Passenger_ID) REFERENCES passenger(passenger_id)
 );
 
 CREATE TABLE stop (
-
+  FOREIGN KEY (Station_A_ID) REFERENCES station(station_id),
+  FOREIGN KEY (Station_B_ID) REFERENCES station(station_id),
+  distancebetween  INT
+  PRIMARY KEY(Station_A_ID, Station_B_ID)
 );
 
-----------------------------
+CREATE TABLE railline_stop (
+  PRIMARY KEY(StopA_ID, StopB_ID, Railline_ID),
+  FOREIGN KEY (StopA_ID) REFERENCES stop(Station_A_ID),
+  FOREIGN KEY (StopB_ID) REFERENCES railline(Station_B_ID),
+  FOREIGN KEY (Railline_ID) REFERENCES railline(railline_id),
+);
