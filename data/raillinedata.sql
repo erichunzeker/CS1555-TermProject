738bd6e1ba4b4a11a1fd3abbf5fbd72dff0f6dd4
Brian Choromanski <BrianChoromanski@gmail.com>
2019-04-03 17:42:23 -0400

    Cleaned up data directory

--- data/raillinedata.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO "railline" (railline_id,speedlimit) VALUES (1,98);
-INSERT INTO "railline" (railline_id,speedlimit) VALUES (2,66);
-INSERT INTO "railline" (railline_id,speedlimit) VALUES (3,82);
-INSERT INTO "railline" (railline_id,speedlimit) VALUES (4,74);
-INSERT INTO "railline" (railline_id,speedlimit) VALUES (5,51);
