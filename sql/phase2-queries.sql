ec2e38539020949eb98abb47d102d0112cbe5a57
Eric Hunzeker <emh128@pitt.edu>
2019-04-04 19:27:01 -0400

    fix 1.2.4.1

--- sql/phase2-queries.sql
+++ sql/phase2-queries.sql
@@ -33,7 +33,7 @@ SELECT *
 WITH RECURSIVE sortroute(route_id, stops_at_a, stops_at_b, stop_id, station_a_id, station_b_id) AS (
     SELECT route_id, stops_at_a, stops_at_b, stop.stop_id AS stop_id, station_a_id, station_b_id
     FROM route_stop, stop
-    WHERE route_stop.stop_id = stop.stop_id AND stop.stop_id = (SELECT stop_id FROM route WHERE route_id = 5) AND route_id = 5
+    WHERE route_stop.stop_id = stop.stop_id AND stop.stop_id = (SELECT stop_id FROM route WHERE route_id = 1) AND route_id = 1
   UNION ALL
     SELECT rs.route_id, rs.stops_at_a, rs.stops_at_b, s.stop_id, s.station_a_id, s.station_b_id
     FROM sortroute sr, route_stop rs, stop s
@@ -47,7 +47,7 @@ SELECT * FROM sortroute;
 -- 1.2.3. Note that all trip searches must account for available seats, and only
 -- show results for trains that have available seats.
 
--- add 'WHERE seats_taken < seats;' to any query searching for trips to get available seats 
+-- add 'WHERE seats_taken < seats;' to any query searching for trips to get available seats
 
 SELECT *
   FROM schedule
@@ -56,14 +56,26 @@ SELECT *
   WHERE seats_taken < seats;
 
 -- 1.2.4.1. Fewest stops ( do pagnation in parameterized query in phase 3 (for all of these 1.2.4.x))
-SELECT R.route_id, R.description, count(R.route_id) as stop_count
+SELECT A.route_id, count(Stop_ID) as stop_count
+  FROM
+  (SELECT R.route_id, R.description
   FROM route R
   INNER JOIN route_stop RS
   ON R.route_id = RS.Route_ID
   INNER JOIN stop S
-  ON R.Stop_ID = S.Stop_ID
-    WHERE Station_A_ID = 1 AND Station_B_ID = 5 AND Stops_At_B = TRUE
-  GROUP BY R.route_id
+  ON RS.Stop_ID = S.Stop_ID
+    WHERE Station_A_ID = 1 AND Stops_At_A = TRUE
+    INTERSECT
+  SELECT R.route_id, R.description
+  FROM route R
+  INNER JOIN route_stop RS
+  ON R.route_id = RS.Route_ID
+  INNER JOIN stop S
+  ON RS.Stop_ID = S.Stop_ID
+    WHERE Station_B_ID = 5 AND Stops_At_B = TRUE) AS A
+  INNER JOIN route_stop RS
+  ON RS.Route_ID = A.route_id
+  GROUP BY A.route_id
   ORDER BY stop_count ASC;
 
 -- 1.2.4.2. Run through most stations
@@ -203,9 +215,6 @@ SELECT route_id
 
 -- 1.3.4. Find any stations through which all trains pass through: Find any stations that all the trains (that are in the system) pass at any time during an entire week.
 
-/* SELECT *
-  FROM schedule = (SELECT count(*) FROM train)
-
 SELECT *
   FROM schedule S
   INNER JOIN train T
@@ -215,7 +224,7 @@ SELECT *
   INNER JOIN stop
   ON RS.Stop_ID = stop.Stop_ID
   WHERE
-    (SELECT count())= (SELECT count(*) FROM train); */
+    (SELECT count(*) from train) = (SELECT (*) FROM train);
 
 -- 1.3.5. Find all the trains that do not stop at a specific station: Find all trains that do not stop at a specified station at any time during an entire week.
 SELECT DISTINCT S.Train_ID
@@ -241,7 +250,7 @@ SELECT stats.route_id FROM (
 -- 1.3.7 Display the schedule of a route: For a specified route, list the days of departure, departure hours and trains that run it.
 SELECT weekday, runtime, Train_ID
   FROM schedule
-  WHERE Route_ID = 1;
+  WHERE Route_ID = 211;
 
 
 -- 1.3.8 Find the availability of a route at every stop on a specific day and time: Find the number of available seats at each stop of a route for the day and time given as parameters.
